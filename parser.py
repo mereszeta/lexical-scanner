c524fc8f733792eaf76c7f604e992bc9b01c60af
mereszeta <grzegorzszeremeta@gmail.com>
2018-04-16 02:20:11 +0200

    no errors - just warnings

--- parser.py
+++ parser.py
@@ -42,6 +42,19 @@ def p_instructions_1(p):
 def p_instructions_2(p):
     """instructions : instruction """
 
+
+def p_instruction(p):
+    """ instruction : assign_instruction
+                    | print_instruction
+                    | return_instruction
+                    | break_instruction
+                    | continue_instruction
+                    | for_instruction
+                    | while_instruction
+                    | choice_instruction"""
+    p[0] = p[1]
+
+
 def p_expression_group(p):
     'expression : "(" expression ")"'
     p[0] = p[2]
@@ -53,6 +66,7 @@ def p_assign_instruction(p):
                          | var assign_operand ID
                          | var assign_operand var
                          | var assign_operand expression
+                         | ID "=" matrix_init_instruction
     '''
     p[0] = AssignInstruction(p[2], p[1], p[3])
 
@@ -78,7 +92,7 @@ def p_assign_operand(p):
                       | DIVASSIGN
     '''
     p[0] = p[1]
-    print("INSTRUCTION ASSIGN")
+
 
 def p_expression_binop(p):
     '''expression : expression "+" expression
@@ -103,6 +117,7 @@ def p_expression_number(p):
     'expression : INTNUM'
     p[0] = Number(p[1])
 
+
 def p_exp_name(p):
     'expression : ID'
     try:
@@ -110,6 +125,7 @@ def p_exp_name(p):
     except LookupError:
         print("Name not defined")
         p[0] = 0
+
 
 def p_expression_unop(p):
     '''expression : "-" expression
@@ -126,33 +142,41 @@ def p_expression_block(p):
     else:
         p[0] = p[1]
 
-
+def p_choice_instruction(p):
+    """choice_instruction : if_else
+                          | if """
 def p_if_else(p):
-    'instruction : IF "(" expression ")" block ELSE block'
+    'if_else : IF "(" expression ")" block ELSE block'
     p[0] = IfElse(p[3], p[5], p[7])
 
+
 def p_if(p):
-    'instruction : IF "(" expression ")" block'
+    'if : IF "(" expression ")" block'
     p[0] = IfElse(p[3], p[5], None)
 
-
+def p_for_instruction(p):
+    """for_instruction : for_1
+                       | for_2"""
 def p_for_1(p):
-    'instruction : FOR ID "=" INTNUM ":" INTNUM "{" instructions "}"'
+    'for_1 : FOR ID "=" INTNUM ":" INTNUM "{" instructions "}"'
     p[0] = ForLoop(p[2], p[4], p[6], p[8])
 
 
 def p_for_2(p):
-    'instruction : FOR ID "=" INTNUM ":" INTNUM instruction '
+    'for_2 : FOR ID "=" INTNUM ":" INTNUM instruction '
     p[0] = ForLoop(p[2], p[4], p[6], p[7])
 
+def p_while_instruction(p):
+    """while_instruction : while_1
+                         | while_2"""
 
 def p_while_1(p):
-    'instruction : WHILE "(" expression ")" instruction'
+    'while_1 : WHILE "(" expression ")" instruction'
     p[0] = WhileLoop(p[3], p[5])
 
 
 def p_while_2(p):
-    'instruction : WHILE "(" expression ")" "{" instructions "}"'
+    'while_2 : WHILE "(" expression ")" "{" instructions "}"'
     p[0] = WhileLoop(p[3], p[6])
 
 
@@ -171,23 +195,23 @@ def p_eye(p):
     p[0] = Eye(p[3])
 
 
-def p_break(p):
-    'instruction : BREAK ";"'
+def p_break_instruction(p):
+    'break_instruction : BREAK ";"'
     p[0] = BreakInstruction()
 
 
-def p_return(p):
-    'instruction : RETURN ";"'
+def p_return_instruction(p):
+    'return_instruction : RETURN ";"'
     p[0] = ReturnInstruction()
 
 
-def p_continue(p):
-    'instruction : CONTINUE ";"'
+def p_continue_instruction(p):
+    'continue_instruction : CONTINUE ";"'
     p[0] = ContinueInstruction()
 
 
-def p_print(p):
-    'instruction : PRINT expression ";"'
+def p_print_instruction(p):
+    'print_instruction : PRINT expression ";"'
     p[0] = PrintInstruction(p[2])
 
 
@@ -198,8 +222,7 @@ def p_array_range(p):
 
 def p_range(p):
     'range : "[" INTNUM "," INTNUM "]" '
-    p[0]= Range(p[2], p[4])
-
+    p[0] = Range(p[2], p[4])
 
 
 def p_matrix_init_instruction(p):
@@ -207,9 +230,10 @@ def p_matrix_init_instruction(p):
                                | '[' matrix_row ']'
                                | '[' matrix_rows ']'
     '''
-    if len(p)==2:
+    if len(p) == 2:
         p[0] = p[1]
-    else: p[0] = p[2]
+    else:
+        p[0] = p[2]
 
 
 def p_matrix_init_fun(p):
@@ -222,10 +246,11 @@ def p_matrix_init_fun(p):
 def p_matrix_row(p):
     """matrix_row : matrix_row ',' INTNUM
                       | INTNUM """
-    if len(p)==4:
+    if len(p) == 4:
         if p[1] is None:
-            p[0]=Row()
-        else: p[0]=p[1]
+            p[0] = Row()
+        else:
+            p[0] = p[1]
         p[0].append(p[3])
     else:
         p[0].append(p[3])
@@ -234,12 +259,14 @@ def p_matrix_row(p):
 def p_matrix_rows(p):
     """matrix_rows : matrix_row ';' matrix_rows
                    | matrix_row"""
-    if len(p)==4:
+    if len(p) == 4:
         if p[1] is None:
-            p[0]=Matrix()
-        else: p[0]=p[1]
+            p[0] = Matrix()
+        else:
+            p[0] = p[1]
         p[0].append(p[3])
     else:
         p[0].append(p[3])
+
 
 parser = yacc.yacc()
